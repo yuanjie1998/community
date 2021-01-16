1c8bc89377a580f492eb47316e56b6e2c0aa335e
yuanjie <1916819402@qq.com>
2021-01-16 23:14:37 +0800

    追加文件2

--- src/main/java/com/nowcoder/community/config/MongoConfig.java
+++ src/main/java/com/nowcoder/community/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.config;
 
 public class MongoConfig {
     public static void main(String[] args) {
+        ///对哦我分不清我对不起我兄
         System.out.println("测试上传到github");
     }
 }
